7ea17ce91d36b0e8ec1c04b01dc34630acabe466
Lucas Ferreira <lsferreirapel@gmail.com>
2021-02-23 20:48:05 -0300

    :art: Create "CompletedChallenges" component
    AULA 02: Create completed challenges component and fix font bug

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,6 +3,7 @@ import { InfoSection } from '../styles/pages/StyledHome';
 import Container from '../styles/global/Container';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
+import CompletedChallenges from '../components/CompletedChallenges';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <InfoSection>
         <div className="left-container">
           <Profile />
+          <CompletedChallenges />
         </div>
 
         <div className="right-container">
